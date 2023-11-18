3305cb895ee5f958037e0d063eca069bea394c34
Eric Feitosa <eric_feitosa13@hotmail.com>
2023-11-17 22:28:15 -0300

    Move games on list

--- src/main/java/com/ericdev/dslist/services/GameService.java
+++ src/main/java/com/ericdev/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.ericdev.dslist.dto.GameDTO;
 import com.ericdev.dslist.dto.GameMinDTO;
 import com.ericdev.dslist.entities.Game;
+import com.ericdev.dslist.projections.GameMinProjection;
 import com.ericdev.dslist.repositories.GameRepository;
 
 @Component
@@ -28,6 +29,13 @@ public class GameService {
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto; 
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+		
 	}
 
 }
